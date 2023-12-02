72add7d4d22f3727ce6cae3ae288a4a70eb695a5
zgagnon <zoe@zgagnon.com>
2023-12-01 20:24:06 -0500

    Simplify and double check
    Check once for digit parsibility of the next char
     rather than for each possible digit
    
    save input instead of copy/paste

--- day-1/day-1.hs
+++ day-1/day-1.hs
@@ -11,31 +11,37 @@ import Data.Function
 import Debug.Trace
 
 parse :: (String -> Char -> String) -> ([Char] -> [Char] -> Bool) -> (String, String) -> (String, String)
-parse subStringer compare (x:xs, acc) = let sub = trace ([x] ++ " " ++ xs ++ " " ++ acc) (subStringer acc x) in
- if sub == "one" || x == '1' then ("1", "")
- else if sub == "two" || x == '2' then ("2", "")
- else if sub == "three" || x == '3' then ("3", "")
- else if sub == "four" || x == '4' then ("4", "")
- else if sub == "five" || x == '5' then ("5", "")
- else if sub == "six" || x == '6' then ("6", "")
- else if sub == "seven" || x == '7' then ("7", "")
- else if sub == "eight" || x == '8' then ("8", "")
- else if sub == "nine" || x == '9' then ("9", "")
- else if compare sub "one"
-  || compare sub "two"
-  || compare sub "three"
-  || compare sub "four"
-  || compare sub "five"
-  || compare sub "six"
-  || compare sub "seven"
-  || compare sub "eight"
-  || compare sub "nine"
-  then parse subStringer compare (xs, sub)
-  else parse subStringer compare (xs, [x])
+parse subStringer compare (x:xs, acc) = let sub =  (subStringer acc x) in
+  case (reads [x]) :: [(Int, String)] of
+      [( _, "")] -> ([x], "")
+      _         -> if sub == "one" || x == '1' then ("1", "")
+      else if sub == "two" then ("2", "")
+      else if sub == "three" then ("3", "")
+      else if sub == "four"  then ("4", "")
+      else if sub == "five" then ("5", "")
+      else if sub == "six"  then ("6", "")
+      else if sub == "seven"  then ("7", "")
+      else if sub == "eight"  then ("8", "")
+      else if sub == "nine"  then ("9", "")
+      else if compare sub "one"
+        || compare sub "two"
+        || compare sub "three"
+        || compare sub "four"
+        || compare sub "five"
+        || compare sub "six"
+        || compare sub "seven"
+        || compare sub "eight"
+        || compare sub "nine"
+        then parse subStringer compare (xs, sub)
+        else parse subStringer compare (xs, [x])
 
+-- Build from the front, so we add new characters to the end of the substring
+-- and check if the substring is a prefix of the target string
 forwards :: (String, String) -> (String, String)
 forwards (x:xs, acc) = parse (\acc x -> acc ++ [x]) isPrefixOf (x:xs, acc)
 
+-- Build from the back, so we add new characters to the front of the substring
+-- and check if the substring is a suffix of the target string
 backwards :: (String, String) -> (String, String)
 backwards (x:xs, acc) = parse (\acc x -> [x] ++ acc) isSuffixOf (x:xs, acc)
  
@@ -47,7 +53,7 @@ pair x = let
 
 main :: IO ()
 main = do
-  contents <- readFile "./day-1/day-1.input"
+  contents <- readFile "./day-1/day-1.input.txt"
   let split = splitOn "\n" contents
   let parsed = map pair split
   let sums = sum parsed
