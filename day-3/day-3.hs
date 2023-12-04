1336a1c04dadad086a6752469a0e24f6902cfc55
zgagnon <zoe@zgagnon.com>
2023-12-04 01:04:32 -0500

    Stylish

--- day-3/day-3.hs
+++ day-3/day-3.hs
@@ -9,16 +9,16 @@
         build-depends:
             base,
             base-compat
-            
+
 
 -}
 
-import           Debug.Trace
 import           Data.Char
-import          Data.List
 import           Data.Foldable
-import          Data.Function
- 
+import           Data.Function
+import           Data.List
+import           Debug.Trace
+
 
 main :: IO ()
 main = do
@@ -26,7 +26,7 @@ main = do
     let grid = zip [0..] $ map (zip [0..]) contents
     let symbols = findSymbols grid
     let partNums = findParts grid
-    let actualParts = filter (isPart symbols) partNums 
+    let actualParts = filter (isPart symbols) partNums
     let gearRatios = do
             symbol <- symbols
             return $ findGearRatio actualParts symbol
@@ -34,7 +34,7 @@ main = do
         >>= (\part -> [read (num part) :: Int])
         & sum
     print $ sum gearRatios
-   
+
 
 isPartMarker :: Char -> Bool
 isPartMarker c = (not $ isDigit c) && c /= '.'
@@ -43,7 +43,7 @@ type Grid  = [(Int, [(Int, Char)])]
 type Symbol = (Int, Int, Char)
 
 isNeighbors :: PartBuilder -> (Int, Int) -> Bool
-isNeighbors PartBuilder {tl=(tlRow, tlCol), br=(brRow, brCol)} (row, col) = 
+isNeighbors PartBuilder {tl=(tlRow, tlCol), br=(brRow, brCol)} (row, col) =
     tlRow <= row && tlCol <= col && brRow >= row && brCol >= col
 
 isPart :: [Symbol] -> PartBuilder -> Bool
@@ -51,10 +51,10 @@ isPart symbols part = let
     (tlRow, tlCol) = tl part
     (brRow, brCol) = br part
     in
-        foldl (\acc (row, col,_) -> 
+        foldl (\acc (row, col,_) ->
             acc || isNeighbors part (row, col)
         ) False symbols
-    
+
 symbolLocation :: Symbol -> Maybe (Int, Int)
 symbolLocation (x, y, character) = case character of
     isDigit -> Nothing
@@ -62,27 +62,27 @@ symbolLocation (x, y, character) = case character of
     _       -> Just (x, y)
 
 findSymbols  ::  Grid -> [Symbol]
-findSymbols diagram = do 
+findSymbols diagram = do
         (rowIndex, row) <- diagram
         (colIndex, ch) <- row
         return (rowIndex, colIndex, ch)
-    & filter (\(_,_,ch) -> isPartMarker ch) 
-        
+    & filter (\(_,_,ch) -> isPartMarker ch)
+
 data PartBuilder = PartBuilder {tl::(Int, Int), br::(Int, Int), num::String, building::Bool} deriving Show
 
 buildPart :: Int -> [PartBuilder] -> (Int, Char) -> [PartBuilder]
-buildPart row acc (col, char) =  
+buildPart row acc (col, char) =
     let (partBuilder, rest) = case acc of
             [] -> (PartBuilder {tl=(0,0), br=(0,0), num=[], building=False}, [])
             (x:xs) -> (x, xs)
-    in 
+    in
         if isDigit char
         then case building partBuilder of
             False ->  PartBuilder {num = [char], tl=(row - 1, col - 1 ), br=(row + 1, col + 1), building=True} : acc
             True -> partBuilder {num = num partBuilder ++ [char], br=(row + 1, col + 1)} : rest
         else case building partBuilder of
             False -> acc
-            True -> partBuilder {building=False} : rest
+            True  -> partBuilder {building=False} : rest
 
 findParts :: Grid -> [PartBuilder]
 findParts diagram = concat  $ map (\(rowIndex, row) ->
@@ -91,7 +91,7 @@ findParts diagram = concat  $ map (\(rowIndex, row) ->
 findGearRatio :: [PartBuilder] -> Symbol -> Int
 findGearRatio parts (row, col, ch) =
     case ch of
-    '*' ->  let 
+    '*' ->  let
             neighbors = filter (\p -> isNeighbors p (row, col)) parts
         in
         case length neighbors of
@@ -101,4 +101,4 @@ findGearRatio parts (row, col, ch) =
                 in
                     first * second
             _ -> 0
-    _   -> 0 
+    _   -> 0
