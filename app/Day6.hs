6a64cac0e973a96ac193a716047167cbd2dee62f
Zoe Gagnon <zoe@zgagnon.com>
2023-12-06 22:00:38 -0500

    Day 6 - I am so angry about this

---
+++ app/Day6.hs
@@ -0,0 +1,24 @@
+import Data.List.Split (splitOn)
+
+main :: IO ()
+main = do
+    content <- lines <$> readFile "inputs/day-6.input"
+    let races = parseRaces content
+    let counts = do
+            race <- races
+            let plan = planRace race
+            let count = length plan
+            return count
+    print $ product counts
+
+stringToInt :: String -> Int
+stringToInt s = read s :: Int
+
+parseRaces :: [String] -> [(Int, Int)]
+parseRaces races = let
+        t = stringToInt $ concat $ tail $ filter (/= "") $ splitOn " " $ head races
+        d = stringToInt $ concat $ tail $ filter (/= "") $ splitOn " " $ races !! 1
+    in [(t, d)]
+
+planRace :: (Int, Int) -> [Int]
+planRace (t, r) = filter (> r) $ map (\n -> (t-n)*n) [0..t]
