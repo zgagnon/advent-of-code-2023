d4e5896c78a6d99b15a37936281c31327a58beaa
Zoe Gagnon <zoe@zgagnon.com>
2023-12-09 14:14:41 -0500

    Day 8 - took the hint

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,27 @@
 module Main where
 
+import           Data.List.Split    (splitOn)
+import           Day1
 import           Day7
-import          Day1
-import           Data.List.Split (splitOn)
-import System.Environment   
+import           Day8
+import           System.Environment
 
 main :: IO ()
 main = do
-    args <- getArgs 
+    args <- getArgs
     let day = head args
     let part = args !! 1
     let file = args !! 2
     file <- lines <$> readFile file
     case day of
-        "1" -> case part of 
+        "1" -> case part of
           "1" -> print $ Day1.part1 file
           "2" -> print $ Day1.part1 file
-        "7" -> case part of 
+        "7" -> case part of
           "1" -> print $ playDay7Part1 file
           "2" -> print $ playDay7Part2 file
+        "8" -> case part of
+          "1" -> print $ playDay8Part1 file
+          "2" -> print $ playDay8Part2 file
         _   -> putStrLn "No such day"
+
